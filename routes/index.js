a564c19f95f90daa7dd59db972e0c93d37d8ccc7
iwillwen <willwengunn@gmail.com>
2017-06-15 15:59:33 +0800

    Redirect to www.staticfile.org

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 exports.index = function (req, res) {
-  res.end('staticfile.org api service!');
-};
 
-exports.api = require('./api');
+  if (req.headers.host === 'staticfile.org') {
+    return res.redirect('https://www.staticfile.org')
+  }
+
+  res.end('staticfile.org api service!')
+}
+
+exports.api = require('./api')
